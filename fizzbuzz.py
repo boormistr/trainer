463e4b65869d87ae995565355f5df86441fa30f7
Alexey Apukhtin <alex.apukhtin@gmail.com>
2020-06-27 19:18:54 +0300

    FizzBuzz task

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+a, b = input().split(' ')
+for i in range(int(a), int(b) +1):
+    if i % 15 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
